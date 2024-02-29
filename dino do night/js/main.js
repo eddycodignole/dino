5f22b929665f08a7fcfaae3739a823766784c467
eddycodignole <131192323+eddycodignole@users.noreply.github.com>
2024-02-28 23:37:31 -0300

    Add files via upload

---
+++ dino do night/js/main.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const accordionItems = document.querySelectorAll('.accordionItem');
+
+    accordionItems.forEach(item => {
+        const title = item.querySelector('.accordionTitle');
+        title.addEventListener('click', () => {
+            const isActive = item.classList.contains('active');
+            closeAllItems();
+            if (!isActive) {
+                item.classList.add('active');
+            }
+        });
+    });
+
+    function closeAllItems() {
+        accordionItems.forEach(item => {
+            item.classList.remove('active');
+        });
+    }
+});
+
+function toggleMenu() {
+    var lateralMenu = document.querySelector('.lateralMenu');
+    var pageContent = document.querySelector('.pageContent');
+    pageContent.classList.toggle('close');
+    lateralMenu.classList.toggle('active');
+}
